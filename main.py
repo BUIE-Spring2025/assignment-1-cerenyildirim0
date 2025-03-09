c0d2dbd3766a20bed483baf4c81bf59eb48f3303
cerenyildirim0 <cerenyildirim0808@gmail.com>
2025-03-10 00:50:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 def int_to_roman(num):
+  roman_numbers = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),(100, 'C'), 
+                   (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+    
+    roman_str = ""
+    for i, j in roman_numbers:
+        if num >= i:
+           roman_str += j* (num//i)
+           num = num % i
+     
+
+        
+    return roman_str
     """
     Convert an integer to a Roman numeral.
 
